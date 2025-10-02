44a18af184e9fffbef9b86964e0393518a996c28
Quentin PAYET <quentinpayet1203@gmail.com>
2025-10-02 13:58:24 +0200

    installation de primeVue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import Button from "primevue/button"
 </script>
 
 <template>
   <header>
     <div>HEADER</div>
+    <Button label="ceci est un bouton"></Button>
     <!-- <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +11,11 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
 app.use(router)
 
 app.mount('#app')
