d21a48c430ed97387aad33402d12fa96248d0463
lrequena-iut90 <l.requena@outlook.fr>
2025-10-03 10:30:27 +0200

    création d'une première navbar primevue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,33 @@
-
 <script setup>
-import Button from "primevue/button"
+import { ref } from 'vue'
+import Menubar from 'primevue/menubar'
+import Button from 'primevue/button'
+
+// menu model
+const items = ref([
+  { label: 'accueil' },
+  {
+    label: 'prestataire',
+    items: [
+      { label: 'page 1' },
+      { label: 'page 2' },
+    ],
+  },
+  {
+    label: 'organisateur',
+    items: [
+      { label: 'page 1' },
+      { label: 'page 2' },
+    ],
+  },
+])
 </script>
 
 <template>
-  <Button>se connecter</Button>
+  <Menubar :model="items">
+    <template #end>
+      <Button label="Se connecter" class="p-button-outlined" />
+    </template>
+  </Menubar>
 </template>
+
