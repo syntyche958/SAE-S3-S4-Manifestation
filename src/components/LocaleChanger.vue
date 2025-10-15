4d70fedfe9fb7fa9073c04850f1d33060826ae14
Quentin PAYET <quentinpayet1203@gmail.com>
2025-10-15 08:21:01 +0200

    LocalChanger now use PrimeVue Select component

--- src/components/LocaleChanger.vue
+++ src/components/LocaleChanger.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="locale-changer">
-    <select v-model="$i18n.locale">
-      <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}
-      </option>
-    </select>
+    <Select v-model="$i18n.locale" :options="$i18n.availableLocales" size="small">
+    </Select>
   </div>
 </template>
+
+<script setup>
+import { Select } from 'primevue';
+</script>
