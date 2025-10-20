8d4beb31e3500d9f03c30675328e26cd415f5f4c
Quentin PAYET <quentinpayet1203@gmail.com>
2025-10-20 13:26:25 +0200

    commented Preset.js added

---
+++ src/assets/Preset.js
@@ -0,0 +1,25 @@
+// import { definePreset } from '@primeuix/themes'
+// import Aura from '@primeuix/themes/aura'
+
+// const Preset = definePreset(Aura, {
+//   semantic: {
+//     colorScheme: {
+//       light: {
+//         primary: {
+//           color: '#142557',
+//           contrastColor: '#ffffff',
+//           hoverColor: '#142557',
+//           activeColor: '#142557',
+//         },
+//         highlight: {
+//           background: '#142557',
+//           focusBackground: '#142557',
+//           color: '#ffffff',
+//           focusColor: '#ffffff',
+//         },
+//       },
+//     },
+//   },
+// })
+
+// export default Preset
