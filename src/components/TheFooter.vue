2be8e11ea93044a7ffcf9c8feb5ba70f8fa13ee5
Quentin PAYET <quentinpayet1203@gmail.com>
2025-10-31 14:15:12 +0100

    TheFooter.vue => visual enhanced

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,19 +1,16 @@
 <script setup>
-import NewProviderForm from './homeComponents/NewProviderForm.vue';
-import ContactForm from './providerComponents/ContactForm.vue';
+import { Divider } from 'primevue'
+
+import NewProviderForm from './homeComponents/NewProviderForm.vue'
+import ContactForm from './providerComponents/ContactForm.vue'
 </script>
 
 <template>
-  <div id="footer">
-    <ContactForm />
-    <NewProviderForm />
+  <div class="w-full">
+    <Divider type="solid" />
+    <div class="flex justify-center gap-2 mb-4">
+      <ContactForm />
+      <NewProviderForm />
+    </div>
   </div>
 </template>
-
-<style scoped>
-#footer {
-  background-color: aquamarine;
-  width: 100%;
-  height: 30vh;
-}
-</style>
