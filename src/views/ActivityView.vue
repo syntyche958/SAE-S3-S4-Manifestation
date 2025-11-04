8ad0388e4fdc308c71381616b12fa7e8312d345f
Quentin PAYET <quentinpayet1203@gmail.com>
2025-11-04 09:59:45 +0100

    multiple activityComponents deleted

--- src/views/ActivityView.vue
+++ src/views/ActivityView.vue
@@ -1,20 +1,4 @@
 <template>
-  <!-- <div style="display: flex; gap: 20px">
-    <div>
-      <SideBarMenuActivity />
-    </div>
-    <div style="display: flex; flex-direction: column; justify-content: flex-start; gap: 20px">
-      <PresentationActivitySection />
-
-      <DescriptionActivitySection />
-
-      <div style="margin: auto">
-        <h2>Emplacement de l'activité</h2>
-        <TheMap :display-mode="MapModeEnum.VISITOR" />
-      </div>
-    </div>
-  </div> -->
-
   <!-- Provider(owner) / Admin -->
   <div
     v-if="
@@ -30,16 +14,21 @@
       <TabPanels>
         <TabPanel value="0">
           <h1>Presentation de l'activité</h1>
+          <h1>S'inscrire</h1>
         </TabPanel>
 
         <TabPanel value="1">
-          <TheMap />
+          <TheMap :display-mode="MapModeEnum.VISITOR" />
         </TabPanel>
       </TabPanels>
     </Tabs>
   </div>
+
   <!-- Visitor -->
-  <div v-else><h1>Presentation de l'activité</h1></div>
+  <div v-else>
+    <h1>Presentation de l'activité</h1>
+    <h1>S'inscrire</h1>
+  </div>
 </template>
 
 <script setup>
@@ -52,12 +41,9 @@ import Tab from 'primevue/tab'
 import TabPanels from 'primevue/tabpanels'
 import TabPanel from 'primevue/tabpanel'
 
-// import PresentationActivitySection from '@/components/activityComponents/PresentationActivitySection.vue'
-// import SideBarMenuActivity from '@/components/activityComponents/SideBarMenuActivity.vue'
-// import DescriptionActivitySection from '@/components/activityComponents/DescriptionActivitySection.vue'
 import TheMap from '@/components/TheMap.vue'
-// import { MapModeEnum } from '@/enums/Map.enums.js'
 import { useProviderStore } from '@/stores/providers'
+import { MapModeEnum } from '@/enums/Map.enums'
 
 const authStore = useAuthStore()
 const providerStore = useProviderStore()
