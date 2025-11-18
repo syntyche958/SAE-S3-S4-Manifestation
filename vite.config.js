b3cea8289527365d153a6b0c935068934018a881
Quentin PAYET <quentinpayet1203@gmail.com>
2025-11-18 07:29:13 +0100

    fix heroku deployement

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,12 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-  ],
+export default defineConfig(({ mode }) => ({
+  plugins: [vue(), ...(mode === 'development' ? [vueDevTools()] : []), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}))
