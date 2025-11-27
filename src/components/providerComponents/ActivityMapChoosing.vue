a04dd3be8fe88552f7dfaa46d49bb11c66ce5355
Quentin PAYET <quentinpayet1203@gmail.com>
2025-11-27 16:58:28 +0100

    wip

--- src/components/providerComponents/ActivityMapChoosing.vue
+++ src/components/providerComponents/ActivityMapChoosing.vue
@@ -1,24 +1,34 @@
 <template>
   <div class="flex gap-6">
+    <!-- Left panel -->
     <TheMap
       :display-mode="MapModeEnum.PROVIDER"
-      @change-selected-location="
-        (locationId) => {
-          selectedLocationId = locationId
-        }
-      "
+      @change-selected-location="onChangeSelectedLocation"
       class="w-fit"
     />
+
+    <!-- Right panel -->
     <Card v-if="selectedLocation != null">
-      <template #title>Caractéritiques de l'emplacement séléctionné</template>
+      <template #title>Caractéristiques de l'emplacement séléctionné</template>
       <template #content>
         <div>Surface : {{ selectedLocation.surfaceArea }}</div>
         <div>Electricité: {{ selectedLocation.electricity == true ? 'oui' : 'non' }}</div>
         <div>Eau: {{ selectedLocation.water == true ? 'oui' : 'non' }}</div>
-        <!-- TODO : Bien faire attention: une seule demande possible par activité -->
-        <!-- Donc si une autre demande existe déjà, le supprimer ! -->
         <!-- TODO : Afficher les demandes dans la partie admin en enlevant le tabs juste tout mettre en colonne avec un tableau liste des demandes avec un bouton 'accepter' -->
-        <Button label="Demander l'emplacement" @click="askLocation"></Button
+        <!-- TODO : Empecher de demander un emplacement quand un lui est déjà assigné ! -->
+        <Button
+          v-if="isAlreadyAsked"
+          label="Demande déjà enregistré"
+          severity="info"
+          variant="text"
+          disabled />
+        <Button
+          v-else-if="isAlreadyTaken"
+          label="Emplacement déjà réservé"
+          severity="danger"
+          variant="text"
+          disabled />
+        <Button v-else label="Demander l'emplacement" @click="askLocation"></Button
       ></template>
     </Card>
     <Card v-else
@@ -47,6 +57,21 @@ const selectedLocationId = ref()
 const selectedLocation = computed(() =>
   locationStore.locations.find((l) => l.id === selectedLocationId.value),
 )
+
+const isAlreadyAsked = computed(() => {
+  const currentActivityId = Number(route.params.activity_id)
+  return activityStore.get(currentActivityId).requestedLocationId === selectedLocationId.value
+})
+
+const isAlreadyTaken = computed(() => {
+  return (
+    activityStore.activities.filter((a) => a.locationId === selectedLocationId.value).length != 0
+  )
+})
+
+const onChangeSelectedLocation = (locationId) => {
+  selectedLocationId.value = locationId
+}
 
 function askLocation() {
   const currentActivityId = Number(route.params.activity_id)
