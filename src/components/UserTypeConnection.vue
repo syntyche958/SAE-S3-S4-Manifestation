65f8f7ec055687c9caf3da323c117d201e2b54fd
Quentin PAYET <quentinpayet1203@gmail.com>
2025-12-04 21:36:34 +0100

    redirect to user page after login (admin or provider)

--- src/components/UserTypeConnection.vue
+++ src/components/UserTypeConnection.vue
@@ -1,12 +1,48 @@
+<template>
+  <ul style="display: flex; flex-direction: row; gap: 20px; align-items: center">
+    <li>
+      <Button
+        label="Administrateur"
+        style="font-size: 20px; padding: 3px 8px"
+        @click="
+          () => {
+            login(UserTypeEnum.ADMIN)
+            router.push('/admin')
+          }
+        "
+      />
+    </li>
+
+    <li>
+      <Select
+        v-model="selectedProvider"
+        :options="items"
+        optionLabel="label"
+        placeholder="Prestataire"
+        @change="
+          (e) => {
+            const providerId = e.value.id
+            login(UserTypeEnum.PROVIDER, providerId)
+            router.push(`/provider/${providerId}`)
+          }
+        "
+      />
+    </li>
+
+    <li><Button label="Visiteur" style="font-size: 20px; padding: 3px 8px" /></li>
+    <li><Button label="Rester déconnecté" style="font-size: 20px; padding: 3px 8px" /></li>
+  </ul>
+</template>
+
 <script setup>
-import Button from 'primevue/button'
-import Dropdown from 'primevue/dropdown'
+import { Button, Select } from 'primevue'
 import { computed, ref } from 'vue'
-// import router from '@/router'
 import { useProviderStore } from '@/stores/providers'
 import { UserTypeEnum } from '@/enums/User.enum.js'
 import { useAuthStore } from '@/stores/auth.js'
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
 const authStore = useAuthStore()
 const providerStore = useProviderStore()
 
@@ -29,28 +65,3 @@ function login(userType, id = undefined) {
   emit('hide-dialog')
 }
 </script>
-
-<template>
-  <ul style="display: flex; flex-direction: row; gap: 20px; align-items: center">
-    <li>
-      <Button
-        label="Administrateur"
-        style="font-size: 20px; padding: 3px 8px"
-        @click="login(UserTypeEnum.ADMIN)"
-      />
-    </li>
-
-    <li>
-      <Dropdown
-        v-model="selectedProvider"
-        :options="items"
-        optionLabel="label"
-        placeholder="Prestataire"
-        @change="(e) => login(UserTypeEnum.PROVIDER, e.value.id)"
-      />
-    </li>
-
-    <li><Button label="Visiteur" style="font-size: 20px; padding: 3px 8px" /></li>
-    <li><Button label="Rester déconnecté" style="font-size: 20px; padding: 3px 8px" /></li>
-  </ul>
-</template>
