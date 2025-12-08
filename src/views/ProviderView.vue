7c4ff2effe5037ec229dc87a2f588a61399cc08b
Quentin PAYET <quentinpayet1203@gmail.com>
2025-12-08 01:33:38 +0100

    enhance visual of providers view as admin

--- src/views/ProviderView.vue
+++ src/views/ProviderView.vue
@@ -6,37 +6,38 @@
   </div>
 
   <!-- Provider / Admin -->
-  <Tabs v-else value="0">
-    <TabList>
-      <Tab value="0"
-        ><i class="pi pi-file-edit"></i><span>{{ $t('message.appearanceOfThePage') }}</span></Tab
-      >
-      <Tab value="1"
-        ><i class="pi pi-file-edit"></i><span>{{ $t('message.chooseServices') }}</span></Tab
-      >
-      <Tab disabled
-        ><i class="pi pi-file-edit"></i><span> {{ $t('message.seeStatistics') }}</span></Tab
-      >
-    </TabList>
+  <div v-else class="flex justify-center">
+    <Card class="w-[95%] mt-[1vh] min-h-[80vh] z-0">
+      <template #content>
+        <Tabs value="0">
+          <TabList>
+            <Tab value="0"
+              ><i class="pi pi-file-edit"></i
+              ><span>{{ $t('message.appearanceOfThePage') }}</span></Tab
+            >
+            <Tab value="1"
+              ><i class="pi pi-file-edit"></i><span>{{ $t('message.chooseServices') }}</span></Tab
+            >
+            <Tab disabled
+              ><i class="pi pi-file-edit"></i><span> {{ $t('message.seeStatistics') }}</span></Tab
+            >
+          </TabList>
 
-    <TabPanels>
-      <TabPanel value="0">
-        <PresentationProviderSection />
-      </TabPanel>
+          <TabPanels>
+            <TabPanel value="0">
+              <PresentationProviderSection />
+            </TabPanel>
 
-      <TabPanel value="1">
-        <ServicesProviderSection />
-      </TabPanel>
-    </TabPanels>
-  </Tabs>
+            <TabPanel value="1">
+              <ServicesProviderSection />
+            </TabPanel>
+          </TabPanels> </Tabs></template
+    ></Card>
+  </div>
 </template>
 
 <script setup>
-import Tabs from 'primevue/tabs'
-import TabList from 'primevue/tablist'
-import Tab from 'primevue/tab'
-import TabPanels from 'primevue/tabpanels'
-import TabPanel from 'primevue/tabpanel'
+import { Card, Tabs, Tab, TabPanel, TabPanels, TabList } from 'primevue'
 
 import PresentationProviderSection from '@/components/providerComponents/PresentationProviderSection.vue'
 import ServicesProviderSection from '@/components/providerComponents/ServicesProviderSection.vue'
