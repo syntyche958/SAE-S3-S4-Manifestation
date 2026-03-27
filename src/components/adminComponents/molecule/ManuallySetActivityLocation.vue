f0a5ffbee2dd26fb1a241b02ddaeec129f05563d
Quentin PAYET <quentinpayet1203@gmail.com>
2026-03-27 09:16:05 +0100

    ManuallySetActivityLocation.vue done working for dynamic reservations

--- src/components/adminComponents/molecule/ManuallySetActivityLocation.vue
+++ src/components/adminComponents/molecule/ManuallySetActivityLocation.vue
@@ -1,51 +1,78 @@
 <template>
   <div class="mt-6 flex flex-col gap-3">
     <h2 class="mb-0!">{{ $t('message.manuallyChoose') + ' :' }}</h2>
-    <Message v-if="alreadySetActivity" severity="warn"
-      >{{ $t('message.locationAlreadyReservedTo') + ' :'
-      }}<b>{{ alreadySetActivity.name }}</b></Message
-    >
-    <Select
-      v-model="selectedActivity"
-      :options="filteredActivities"
-      optionLabel="name"
-      placeholder="Select an activity"
-      class="w-full md:w-56"
-    />
-    <Button
-      type="button"
-      :label="alreadySetActivity ? t('message.modify') : t('message.save')"
-      icon="pi pi-save"
-      @click="setActivityLocation"
-      :disabled="selectedActivity == undefined"
-      class="w-fit"
-    />
+    <Message severity="info">Sélectionnez une activité pour chaque heure.</Message>
+
+    <DataTable :value="timeSlots" dataKey="slotKey" paginator :rows="8">
+      <Column field="beginingDate" header="Date" sortable />
+      <Column field="beginingHour" header="Heure" sortable />
+      <Column header="Activité">
+        <template #body="{ data }">
+          <Select
+            v-model="manualAssignments[data.slotKey]"
+            :options="availableActivities"
+            optionLabel="name"
+            optionValue="id"
+            placeholder="Choisir une activité"
+            class="w-full md:w-72"
+          />
+        </template>
+      </Column>
+      <Column header="">
+        <template #body="{ data }">
+          <Button
+            icon="pi pi-save"
+            size="small"
+            :disabled="!manualAssignments[data.slotKey]"
+            @click="setActivityLocation(data.slotKey, manualAssignments[data.slotKey])"
+          />
+        </template>
+      </Column>
+    </DataTable>
   </div>
 </template>
 
 <script setup>
 import { computed, ref } from 'vue'
-import { Button, Select, Message } from 'primevue'
+import { Select, Message, DataTable, Column, Button } from 'primevue'
 import { useActivityStore } from '@/stores/activities'
 import { useI18n } from 'vue-i18n'
 
-const { t } = useI18n()
+useI18n()
 const activityStore = useActivityStore()
 
 const emit = defineEmits(['set-activity-location'])
-const props = defineProps({
+
+defineProps({
   selectedLocation: { required: true },
 })
 
-const selectedActivity = ref()
-const filteredActivities = computed(() =>
-  activityStore.activities.filter((a) => a.locationId == undefined),
-)
-const alreadySetActivity = computed(() =>
-  activityStore.activities.find((a) => a.locationId === props.selectedLocation.id),
+const manualAssignments = ref({})
+
+const eventDays = ['2026-05-28', '2026-05-29'] // TODO Déplacer
+const startHour = 8
+const endHour = 23
+
+const timeSlots = computed(() => {
+  const slots = []
+  for (const day of eventDays) {
+    for (let h = startHour; h <= endHour; h++) {
+      const hour = `${String(h).padStart(2, '0')}:00`
+      slots.push({
+        slotKey: `${day}T${hour}`,
+        beginingDate: day,
+        beginingHour: hour,
+      })
+    }
+  }
+  return slots
+})
+
+const availableActivities = computed(() =>
+  [...activityStore.activities].sort((a, b) => a.name.localeCompare(b.name)),
 )
 
-const setActivityLocation = () => {
-  emit('set-activity-location', selectedActivity.value.id)
+const setActivityLocation = (slotKey, activityId) => {
+  emit('set-activity-location', { activityId, dateHour: slotKey })
 }
 </script>
