f97a7a622d9c694f384f7a18c2d34de010807496
jpasteur-iut90 <julian.pasteur@edu.univ-fcomte.fr>
2026-03-30 15:30:49 +0200

    Back provider terminés

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 const instance = axios.create({
   baseURL: 'http://localhost:3000',
-  timeout: 1000,
+  timeout: 10000,
+  withCredentials: true,
   //headers: {'X-Custom-Header': 'foobar'}
 })
 
