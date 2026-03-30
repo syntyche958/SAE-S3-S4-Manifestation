f7dc711f2099eed97ffad34c0b799ad034d69ace
lrequena-iut90 <l.requena@outlook.fr>
2026-03-30 15:56:36 +0200

    wip

--- src/stores/registration.js
+++ src/stores/registration.js
@@ -3,18 +3,10 @@ import { defineStore } from 'pinia'
 import registrationService from '@/services/registration.service'
 
 export const useRegistrationStore = defineStore('registration', () => {
-  // STATE
   const registrations = ref([])
 
-  // ACTIONS
-  async function getAllRegistrations() {
-    let response = await registrationService.getAllRegistrations()
-    if (response.error === 0) registrations.value = response.data
-    else console.log(response.data)
-  }
-
-  async function getByActivity(activityId) {
-    let response = await registrationService.getRegistrationsByActivity(activityId)
+  async function getBySession(sessionId) {
+    let response = await registrationService.getRegistrationsBySession(sessionId)
     if (response.error === 0) return response.data
     else {
       console.log(response.data)
@@ -31,14 +23,19 @@ export const useRegistrationStore = defineStore('registration', () => {
     }
   }
 
-  async function addRegistration(activityId, sessionId, userId) {
-    let response = await registrationService.addRegistration(activityId, sessionId, userId)
+  async function addRegistration(sessionId, userId) {
+    let response = await registrationService.addRegistration(sessionId, userId)
     if (response.error === 0) {
-      // Ajouter à l'état local
-      if (!registrations.value) {
-        registrations.value = []
-      }
-      registrations.value.push(response.data)
+      return response.data
+    } else {
+      console.log(response.data)
+      return null
+    }
+  }
+
+  async function deleteRegistration(sessionId, userId) {
+    let response = await registrationService.deleteRegistration(sessionId, userId)
+    if (response.error === 0) {
       return response.data
     } else {
       console.log(response.data)
@@ -47,10 +44,10 @@ export const useRegistrationStore = defineStore('registration', () => {
   }
 
   return {
-    getAllRegistrations,
-    getByActivity,
+    registrations,
+    getBySession,
     getByUser,
     addRegistration,
-    registrations,
+    deleteRegistration,
   }
 })
